2221c3535e046eafc0e8c027d74dd58ad41c6f3a
Abhrajyoti <singhaabhrajyoti3@gmail.com>
2026-07-02 16:55:31 +0530

    Stat-cards redirect to correct pages within the (librarian/member) portal

--- Data/LibraryRepository.cs
+++ Data/LibraryRepository.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
 using Microsoft.EntityFrameworkCore;
-using Lib_Mgmt.Data.Entities;
 using Lib_Mgmt.Models;
 
 namespace Lib_Mgmt.Data
