23270232a5184c17c67f351c907b54df91666f6c
Abhrajyoti <singhaabhrajyoti3@gmail.com>
2026-07-17 21:23:11 +0530

    yes

---
+++ Reporting/Exporters/CsvReportExporter.cs
@@ -0,0 +1,29 @@
+﻿using System.Text;
+
+namespace Lib_Mgmt.Reporting.Exporters
+{
+    public class CsvReportExporter : IReportExporter
+    {
+        public string FormatName => "CSV";
+        public string ContentType => "text/csv";
+        public string FileExtension => "csv";
+
+        public byte[] Export(IEnumerable<string> headers, IEnumerable<IEnumerable<object>> rows)
+        {
+            var sb = new StringBuilder();
+            if (headers != null && headers.Any())
+                sb.AppendLine(string.Join(",", headers.Select(Escape)));
+            foreach (var row in rows)
+                sb.AppendLine(string.Join(",", row.Select(Escape)));
+            return Encoding.UTF8.GetBytes(sb.ToString());
+        }
+
+        private static string Escape(object value)
+        {
+            var s = value?.ToString() ?? "";
+            if (s.Contains(',') || s.Contains('"') || s.Contains('\n') || s.Contains('\r'))
+                s = "\"" + s.Replace("\"", "\"\"") + "\"";
+            return s;
+        }
+    }
+}
---
+++ Reporting/Exporters/IReportExporter.cs
@@ -0,0 +1,10 @@
+﻿namespace Lib_Mgmt.Reporting.Exporters
+{
+    public interface IReportExporter
+    {
+        string FormatName { get; }
+        string ContentType { get; }
+        string FileExtension { get; }
+        byte[] Export(IEnumerable<string> headers, IEnumerable<IEnumerable<object>> rows);
+    }
+}
